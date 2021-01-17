78f693ef08a42c26f99554e78538563cdf605e16
komali100 <komali.arepalli2@mindtree.com>
2021-01-17 14:20:43 +0000

    jenkins

---
+++ 05-Terraform/05-LifeCycle/outputs.tf
@@ -0,0 +1,26 @@
+provider "aws" {
+   region = "us-east-2"
+   version = ">=3.7,<=3.11"
+}
+
+resource "aws_instance" "frontend" {
+  ami           = "ami-0a0ad6b70e61be944"
+  instance_type = "t2.micro"
+  depends_on    = [aws_instance.backend]
+
+Lifecycle{
+
+create_before_destroy= true
+
+}
+
+resource "aws_instance" "backend" {
+  ami           = "ami-0a0ad6b70e61be944"
+  instance_type = "t2.micro"
+  count         = 2 
+
+lifecycle{
+prevent_destroy=true
+}
+
+}
