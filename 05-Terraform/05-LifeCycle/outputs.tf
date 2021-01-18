1e41612d5e4237b86e08d43e91073e794ea12e8a
komali100 <komali.arepalli2@mindtree.com>
2021-01-18 15:00:28 +0000

    jenkins

--- 05-Terraform/05-LifeCycle/outputs.tf
+++ 05-Terraform/05-LifeCycle/outputs.tf
@@ -2,25 +2,22 @@ provider "aws" {
    region = "us-east-2"
    version = ">=3.7,<=3.11"
 }
-
 resource "aws_instance" "frontend" {
   ami           = "ami-0a0ad6b70e61be944"
+#  ami           = "ami-0ebc8f6f580a04647"
   instance_type = "t2.micro"
-  depends_on    = [aws_instance.backend]
-
-Lifecycle{
-
-create_before_destroy= true
-
+  depends_on    = [aws_instance.backend]  
+  lifecycle {
+     create_before_destroy = true
+  } 
 }
 
 resource "aws_instance" "backend" {
   ami           = "ami-0a0ad6b70e61be944"
   instance_type = "t2.micro"
-  count         = 2 
+  count         = 2
 
-lifecycle{
-prevent_destroy=true
-}
-
+  lifecycle { 
+    prevent_destroy = false
+  } 
 }
